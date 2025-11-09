e68c1527053baab1c885f30df30318bef8b3b84f
Abubazar <sediqdude360@gmail.com>
2025-11-09 21:14:56 +0100

    gotten my set of block groups. now to make the actual blocks

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const canvas = document.getElementById("blocks")
 const ctx = canvas.getContext('2d')
 
-
-function drawBlock(x,y,width,height,color,text){
-    ctx.beginPath();
-    ctx.roundRect(x, y, width, height, 10);
-    ctx.fillStyle = color;
-    ctx.fill();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = color;
-    ctx.stroke();
-    ctx.fillStyle = "white";
-    ctx.fillText("fgjhdkgf",x+5,y+10)
+blockGroups ={
+    'control': ['#ff8400ff','#dd7200ff'],
+    'event': ['#e8e000ff','#bfb901ff'],
+    'motion': ['#006affff','#0057d0ff'],
+    'looks': ['#ff0062ff','#bd0048ff'],
+    'sound': ['#bb00ffff','#a400e0ff'],
+    'input': ['#00ff84ff','#00dc71ff'],
+    'operator': ['#71fc00ff','#63db00ff'],
+    'variable': ['#f1551cff','#d93a00ff'],
+    'function': ['#d02121ff','#b61717ff'],
 }
 
-drawBlock(100,100,90,30,"#4C97FF")
+function drawBlock(x,y,group,text,size){
+    ctx.font = size+"px monospace"
+    const txtWidth = ctx.measureText(text).width
+    ctx.beginPath()
+    ctx.roundRect(x, y, txtWidth+(size/2), size+(size/4), size/4)
+    ctx.fillStyle = blockGroups[group][0]
+    ctx.fill()
+    ctx.lineWidth = size/10
+    ctx.strokeStyle = blockGroups[group][1]
+    ctx.stroke()
+    ctx.fillStyle = "white"
+    ctx.fillText(text,x+(size/4),y+(size/1.2))
+}
+
+drawBlock(100,100,"control","yodzho",16)
+
+
+
+
+class CodeBlock{
+    constructor(dd){
+
+    }
+}
