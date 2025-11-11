1615a7ba703dd27911365a221e25f13e33fc378e
Abubazar <sediqdude360@gmail.com>
2025-11-11 14:36:38 +0100

    okayyyyyy the engine is working well. ive figured out input signals. now i have a working demo

--- engine.js
+++ engine.js
@@ -14,15 +14,8 @@ function UpDrawImg(img, x, y, deg = 0, op = 1, flipX = 1, flipY = 1, width, heig
     ctx.translate(x, y);
     ctx.scale(scaleX, scaleY);
     ctx.rotate(angle);
-    ctx.globalAlpha = op;
-
-    if (args.length === 4) {
-      const [sx, sy, sw, sh] = args;
-      ctx.drawImage(img, sx, sy, sw, sh, -width / 2, -height / 2, width, height);
-    } else {
-      ctx.drawImage(img, -width / 2, -height / 2, width, height);
-    }
-
+    ctx.globalAlpha = op/100;
+    ctx.drawImage(img, -width / 2, -height / 2, width, height);
     ctx.globalAlpha = 1;
     ctx.restore();
   }
@@ -37,17 +30,78 @@ const FPS = 60
 const timestep = 1000/FPS
 let lastTime = 0
 
+class KeyboardManager {
+    constructor() {
+        this.keys = {}
+
+        window.addEventListener('keydown', e => {
+            this.keys[e.key.toLowerCase()] = true
+        })
+
+        window.addEventListener('keyup', e => {
+            this.keys[e.key.toLowerCase()] = false
+        })
+    }
+
+    isDown(key) {
+        return !!this.keys[key.toLowerCase()]
+    }
+}
+
+class Sprite{
+    constructor(name,x,y,skins){
+        this.name = name
+        this.x = x
+        this.y = y
+        this.angle = 0
+        this.opacity = 100
+        this.scaleX = 1
+        this.scaleY = 1
+        this.skins = skins
+        this.currentSkin = Object.keys(skins)[0];
+    }
+
+    render(){
+        UpDrawImg(this.skins[this.currentSkin],this.x, this.y, this.angle, this.opacity, this.scaleX, this.scaleY,70,70)
+    }
+}
+
+
+
+
+
+
+
+
+
+keyboard = new KeyboardManager()
+
 let coco = 0
+const player = new Sprite("Daniel",100,100,{'skin1':tooli})
+
 function update(delta){
-    coco+=delta*900
+    if(keyboard.isDown('arrowleft')){
+        player.x-=delta*100
+    }
+    else if(keyboard.isDown('arrowright')){
+        player.x+=delta*100
+    }
+
+    if(keyboard.isDown('arrowup')){
+        player.y-=delta*100
+    }
+    else if(keyboard.isDown('arrowdown')){
+        player.y+=delta*100
+    }
 }
 
 function render(){
     ctx.fillStyle="white"
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    UpDrawImg(tooli, 90, 90, coco, 1, 1, 1, 70, 70);
+    player.render()
 }
+
+
 
 function gameLoop(currentTime) {
   const deltaTime = (currentTime - lastTime) / 1000
